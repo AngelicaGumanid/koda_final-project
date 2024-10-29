ed13d231aef289cc47f1480a69455233c6faa7bc
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-10-29 14:39:26 +0800

    add: application controller for ensuring appropriate redirection of page after login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+
+  authenticated :user, ->(user) { user.admin? } do
+    get 'admin_home', to: 'home#index', as: :admin_home
+  end
+
+  authenticated :user, ->(user) { user.client? } do
+    get 'client_home', to: 'home#index', as: :client_home
+  end
 end
