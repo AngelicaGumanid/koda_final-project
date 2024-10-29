cc9e0147b79df9ebf2e0ceedda626b16421206a6
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-10-29 16:20:54 +0800

    add: validations for user registration [Feature 1]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: { client: 'client', admin: 'admin' }
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :phone, presence: true, phone: { possible: true, country: 'PH' }
+  validates :coins, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :total_deposit, numericality: { greater_than_or_equal_to: 0 }
+  validates :children_members, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 end
